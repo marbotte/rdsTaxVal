3b5a78e5d891651f43942bb8e12409ac176db71a
marbotte <bottinmarius@gmail.com>
2025-08-05 19:10:35 -0500

    initial commit

---
+++ R/main_functions.R
@@ -0,0 +1,33 @@
+formatTaxonomy<-function(obj,currentFormat=c("oneTable","tableByPlot_list"),family="family", genus="genus", epithet="specificEpithet", infraspecies="infraspecificEpithet")
+# Function to create the object that will be used to check for inconsistencies later
+# 1. check whether it is one table, or 2
+# 2. check spaces, indetermined, cf. and aff.
+{
+  
+}
+
+checkSpace <- function(obj,family="family", genus="genus", epithet="specificEpithet", infraspecies="infraspecificEpithet")
+{
+  
+}
+
+indetermitedEtc <- function(obj, family="family", genus="genus", epithet="specificEpithet", infraspecies="infraspecificEpithet")
+# Note: when there are cases of Indet or Morpho, species code should be checked for relevant information
+{
+  
+}  
+  
+unicity <- function(obj, family="family",  genus="genus", epithet="specificEpithet", infraspecies="infraspecificEpithet")
+{
+  
+}
+
+
+
+familtySearchGbif<-function(obj)
+  
+
+genusSearchGbif <- function(obj)
+  
+
+spSearchGbif <- function(obj)
